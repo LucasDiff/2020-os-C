e91c51727d0efbc2bfb6a91d1da65f827effb3c1
Lukáš Németh <lukas.nemeth.2@student.tuke.sk>
2020-10-26 23:22:37 +0100

    Update copymaster.c

--- copymaster.c
+++ copymaster.c
@@ -1,60 +1,77 @@
 #include <stdio.h>
+#include <unistd.h>
+#include <sys/stat.h>
+#include <dirent.h>
+#include <time.h>
+#include <sys/types.h>
+#include <stdbool.h>
+#include <fcntl.h>
+#include <inttypes.h>
 #include <stdlib.h>
 #include <errno.h>
 #include <string.h>
-
 #include "options.h"
 
-
-void FatalError(char c, const char* msg, int exit_status);
 void PrintCopymasterOptions(struct CopymasterOptions* cpm_options);
 
+int getInfileSize(struct CopymasterOptions cpm_options);
 
-int main(int argc, char* argv[])
-{
+void FatalError(char c, const char* msg, int exit_status);
+
+bool fileExists(int fileSize);
+
+int getOutfileSize(struct CopymasterOptions cpm_options);
+
+bool controlCopyRegular(struct CopymasterOptions cpm_options);
+
+bool openfailure(int prvy, int druhy);
+
+
+int main(int argc, char* argv[]) {
+
     struct CopymasterOptions cpm_options = ParseCopymasterOptions(argc, argv);
 
-    //-------------------------------------------------------------------
-    // Kontrola hodnot prepinacov
-    //-------------------------------------------------------------------
+    int c = 0;
 
-    // Vypis hodnot prepinacov odstrante z finalnej verzie
-    
     PrintCopymasterOptions(&cpm_options);
     
-    //-------------------------------------------------------------------
-    // Osetrenie prepinacov pred kopirovanim
-    //-------------------------------------------------------------------
-    
-    if (cpm_options.fast && cpm_options.slow) {
+
+    if (controlCopyRegular(cpm_options)){
+
+	int velkost = getInfileSize(cpm_options);
+
+    if (!fileExists(velkost))
+        FatalError('B', "SUBOR NEEXISTUJE", 21);
+
+    struct stat a;
+
+    stat(cpm_options.infile, &a);
+
+    mode_t infileMode = a.st_mode;
+
+    int prvy = open(cpm_options.infile, O_RDONLY);
+
+    int druhy = open(cpm_options.outfile, O_WRONLY | O_CREAT, infileMode);
+
+    if (openfailure(prvy, druhy))
+        FatalError('B', "INA CHYBA", 21);
+
+    char buff[velkost];
+
+    if (read(prvy, &buff, velkost) == -1 ||  write(druhy, &buff, velkost) == -1)
+        FatalError('B', "INA CHYBA", 21);
+
+    close(prvy);
+    close(druhy);
+	}
+
+    if (cpm_options.fast && cpm_options.slow && c == 0) {
         fprintf(stderr, "CHYBA PREPINACOV\n"); 
         exit(EXIT_FAILURE);
     }
-    
-    // TODO Nezabudnut dalsie kontroly kombinacii prepinacov ...
-    
-    //-------------------------------------------------------------------
-    // Kopirovanie suborov
-    //-------------------------------------------------------------------
-    
-    // TODO Implementovat kopirovanie suborov
-    
-    // cpm_options.infile
-    // cpm_options.outfile
-    
-    //-------------------------------------------------------------------
-    // Vypis adresara
-    //-------------------------------------------------------------------
-    
     if (cpm_options.directory) {
         // TODO Implementovat vypis adresara
     }
-        
-    //-------------------------------------------------------------------
-    // Osetrenie prepinacov po kopirovani
-    //-------------------------------------------------------------------
-    
-    // TODO Implementovat osetrenie prepinacov po kopirovani
     
     return 0;
 }
@@ -67,6 +84,7 @@ void FatalError(char c, const char* msg, int exit_status)
     fprintf(stderr, "%c:%s\n", c, msg);
     exit(exit_status);
 }
+
 
 void PrintCopymasterOptions(struct CopymasterOptions* cpm_options)
 {
@@ -109,4 +127,29 @@ void PrintCopymasterOptions(struct CopymasterOptions* cpm_options)
     printf("truncate:      %d\n", cpm_options->truncate);
     printf("truncate_size: %ld\n", cpm_options->truncate_size);
     printf("sparse:        %d\n", cpm_options->sparse);
+}
+
+bool openfailure(int prvy, int druhy) {
+
+    return prvy == -1 || druhy == -2;
+}
+
+int getInfileSize(struct CopymasterOptions cpm_options) {
+    struct stat a;
+    stat(cpm_options.infile, &a);
+    int velkost = a.st_size;
+    return velkost;
+}
+
+bool controlCopyRegular(struct CopymasterOptions cpm_options) {
+
+    return !cpm_options.fast && !cpm_options.slow && !cpm_options.create &&
+    !cpm_options.overwrite && !cpm_options.append && !cpm_options.lseek &&
+    !cpm_options.directory && !cpm_options.delete_opt && !cpm_options.chmod &&
+    !cpm_options.inode && !cpm_options.umask && !cpm_options.link &&
+    !cpm_options.truncate && !cpm_options.sparse;
+}
+
+bool fileExists(int fileSize) {
+    return fileSize > 0;
 }
